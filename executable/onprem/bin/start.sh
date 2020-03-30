a37cf507d8ecd0f2d8d61bca37cb24cdbb858768
Sergei Astapov <sergei.astapov@sc.com>
2020-03-30 13:29:54 +0800

    - changes

--- executable/onprem/bin/start.sh
+++ executable/onprem/bin/start.sh
@@ -3,6 +3,4 @@
 JAVA=java
 JVM_OPTS="-Xms256m -Xmx1024m"
 
-nohup $JAVA $JVM_OPTS -jar \
-disk-cloud-service.jar \
--Dspring.profiles.active=sit
+nohup $JAVA $JVM_OPTS -jar disk-cloud-service.jar -Dspring.profiles.active=sit &
